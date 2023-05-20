c58f7c796d863d25a2950e3a8d5c85ed5af5267b
Vitória Morais <vitoriamoraisrodriguesm@gmail.com>
2023-05-20 14:32:27 -0300

    Menu não finalizado.

--- src/Program.java
+++ src/Program.java
@@ -15,8 +15,8 @@ public class Program {
 		int numero = sc.nextInt();
 		System.out.print("Conta Poupança [P] ou Conta Empresarial [E]: ");
 		char o = sc.next().charAt(0);
-		System.out.println("--------------------------");
-		
+		System.out.println("----------------------------");
+
 		if (o == 'p' || o == 'P') {
 			System.out.println("------ Conta Poupança ------");
 			System.out.print("Informe seu saldo: ");
@@ -24,7 +24,28 @@ public class Program {
 			double taxaDeJuros = saldo * 2;
 			ContaPoupanca cp = new ContaPoupanca(numero, proprietario, saldo, taxaDeJuros);
 			System.out.print("O seu limite de emprestimo é: " + cp.getTaxaDeJuros());
-			System.out.println("----------------------------");
+
+			//Do while para fluxo de "menu"
+			//Usuário erá escolher uma das opções e quando tiver finalizado retornará ao "menu"
+			//Esse fluxo só será finalizado quando o usuário escolher a opção "Finalizar"
+			do {
+				System.out.println("----------------------------");
+				System.out.println("Sacar     [S] \nDepositar [D] \nFinalizar [F] ");
+				o = sc.next().charAt(0);
+
+				if (o == 's' || o == 'S') {
+
+
+				}else if (o == 'd' || o == 'D') {
+
+				}else if(o == 'f' || o == 'F') {
+				System.out.println("Sessão encerrada.");
+				break;
+				}
+
+			}while(true);
+
+
 		} else if (o == 'e' || o == 'E') {
 			System.out.println("------ Conta Empresarial ------");
 			System.out.print("Informe seu saldo: ");
@@ -33,10 +54,16 @@ public class Program {
 			ContaEmpresarial ce = new ContaEmpresarial(numero, proprietario, saldo, limiteEmprestimo);
 			System.out.println("A sua taxa de juros é: " + ce.getLimiteEmprestimo());
 			System.out.println("-------------------------------");
+
+			//Menu
+			System.out.print("Sacar [S] \n Depositar [D] \n Finalizar [F]: ");
+
+
+
 		} else {
 			System.out.print("Opção inválida.");
 		}
-		
+
 		sc.close();
 
 	}
--- src/Transacao.java
+++ src/Transacao.java
@@ -2,15 +2,15 @@
 public class Transacao {
 
 	private Conta conta;
-	
+
 	public Transacao(Conta conta) {
 		this.conta = conta;
 	}
-	
+
 	public void deposit(double quantia) {
 		conta.deposito(quantia);
 	}
-	
+
 	public void withdraw(double quantia) {
 		conta.retirar(quantia);
 	}
